1712554bd4720d96729dd978bdee7ee8f6f2919d
Dimmed Light <113694089+dimmed-light@users.noreply.github.com>
2023-11-20 21:38:44 +0200

    Testing ping package

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"coffee_shop_backend/ping"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello, world")
+
+	r := gin.Default()
+	r.GET("/ping", ping.Pong)
+	r.Run()
 }
