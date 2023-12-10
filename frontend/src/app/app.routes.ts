2019e076df6fcb7b52ce495964374726159f5430
Statzio <77752782+Statzio@users.noreply.github.com>
2023-12-10 21:53:35 +0200

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { TestPageComponent } from './test-page/test-page.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/homepage', pathMatch: 'full' },
     { path: "test-page", component: TestPageComponent },
     { path: "homepage", component: HomepageComponent },
 
