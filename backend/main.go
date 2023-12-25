c4791b83189d5adb0e2a9ce59566849ad21f713c
dimmed-light <schpiel02@proton.me>
2023-12-25 22:02:16 +0200

    gorm installed, dependency issue prevents backend from starting

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+  	"gorm.io/driver/mysql"
 )
 
 func ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "pong"})
+}
+
+// for testing purposes
+type Help struct {
+	gorm.Model
+	Help_topic_id uint
+	Name string
+	Help_category_id uint
+	Description string
+	Example string
+	Url string
 }
 
 func main() {
@@ -16,7 +30,19 @@ func main() {
 
 	//fmt.Println(cup)
 
-	fmt.Println("hello, world")
+	password := os.Getenv("DB_ROOT_PASSWORD")
+	database_name := "mysql"
+	dsn := "root:" + password + "@tcp(db:3306)/" +
+		database_name + "?charset=utf8mb4&parseTime=True&loc=Local"
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Coudln't connect to database!")
+		return
+	}
+
+	var help Help
+	database.First(&help)
+	fmt.Println(help.Description)
 
 	productController := NewProductController()
 
